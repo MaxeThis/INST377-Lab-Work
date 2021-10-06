c3268d86be872bec44873706afd7ef007e1f9312
Max Mendelson <maxwellhmendelson@yahoo.com>
2021-10-06 18:14:10 -0400

    Completed Lab 6

--- client/lab_6/script.js
+++ client/lab_6/script.js
@@ -1,44 +1,39 @@
-const endpoint = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json'
+async function windowActions() {
+  const endpoint = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
 
-const resturaunts = [];
+  const request = await fetch(endpoint)
+  const resturaunts = await request.json()
 
-
-fetch(endpoint)
-    .then(blob => blob.json())
-    .then(data => resturaunts.push(...data));
-
-
-function findMatches(wordToMatch, resturaunts) {
-    return resturaunts.filter(place => {
-        const regex = new RegExp(wordToMatch, 'gi');
-        return place.name.match(regex) || place.category.match(regex);
+  function findMatches(wordToMatch, resturaunts) {
+    return resturaunts.filter((place) => {
+      const regex = new RegExp(wordToMatch, 'gi');
+      return place.name.match(regex) || place.category.match(regex);
     });
-}
+  }
 
-function displayMatches(){
-    console.log(this.value)
-    console.log(this.value.length)
-    if(this.value.length != 0){
-    const matchArray = findMatches(this.value, resturaunts);
-    const html = matchArray.map(place => {
-        return `
+  function displayMatches(event) {
+    console.log(event.target.value);
+    console.log(event.target.value.length);
+    if (event.target.value.length != 0) {
+      const matchArray = findMatches(event.target.value, resturaunts);
+      const html = matchArray.map((place) => `
       <li>
         ${place.name}, <br> 
         ${place.category} <br> 
         <i>${place.address_line_1} <br> 
         ${place.zip}</i>
       </li>
-    `;
-  }).join('');
-  suggestions.innerHTML = html;
+    `).join('');
+      suggestions.innerHTML = html;
+    } else {
+      suggestions.innerHTML = '';
     }
-    else{
-        suggestions.innerHTML = '';
-        return;
-    }
+  }
+
+  const searchInput = document.querySelector('.search');
+  const suggestions = document.querySelector('.suggestions');
+
+  searchInput.addEventListener('keyup', (evt) => { displayMatches(evt) });
 }
 
-const searchInput = document.querySelector('.search');
-const suggestions = document.querySelector('.suggestions');
-
-searchInput.addEventListener('keyup', displayMatches)
+window.onload = windowActions
